757a0a1549e794ef6cc0bb917d53c9dcbb7e5782
AndrewKalil <1541@holbertonschool.com>
2020-03-14 16:42:46 -0500

    adding functions to be used and structs

--- holberton.h
+++ holberton.h
@@ -6,5 +6,15 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+int character(va_list ap);
+int string(va_list ap);
+int decimal(va_list ap);
+int integer(va_list ap);
+
+typedef struct list
+{
+	char *arg;
+	int (*f_arg)();
+} _printf;
 
 #endif
